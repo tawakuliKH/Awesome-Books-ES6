3efd29b4e041575190f6daa611fd8a784c52421b
tawakuliKH <tawakuli456@gmail.com>
2023-03-07 16:35:43 +0430

    Update index.js and index.html file for new changes

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-import showBooks from './modules/Manipulation';
+// eslint-disable-next-line import/extensions
+import { DateTime } from './modules/luxon.js';
+// eslint-disable-next-line import/extensions
+import showBooks from './modules/Manipulation.js';
+
+const date = document.querySelector('.date');
+setInterval(() => {
+  const now = DateTime.now();
+  date.textContent = now.toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS);
+}, 1000);
 
 showBooks();
